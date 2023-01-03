ae6f1dd176371d0639e450a09b05acb8c0ef66ca
Mya Schroder <80082701+schromya@users.noreply.github.com>
2023-01-03 15:41:07 +0100

    Tested querying from local uaa-robo MongoDB with internalIndex.html. All code is currently commented out. To test further, you need to uncomment the specified code in app.py and internalIndex.html and go to <local IP>/internalIndex.html. Also added .gitignore file to ignore /venv and __pycache__

--- app.py
+++ app.py
@@ -4,11 +4,26 @@ from pymongo import MongoClient
 app = Flask(__name__)
 client = MongoClient()
 
+'''
+Uncomment the following if testing with local MongoDB uaa-robo downloaded from 
+https://drive.google.com/drive/folders/1AdAEVCsk90r3HB74M-stUP_JhE84dCHs?usp=sharing
+There is also more to uncomment futher below and  in internalIndex.html
+'''
+
+'''
+client = MongoClient('localhost', 27017) #For testing with local mongo db. 27017 is the proper port
+db = client['uaa-robo'] #Same as db = client.uaa-robo (but python doesn't like the uaa-robo format)
+person = db.Person
+'''
+
+
+
 
 """
 Static Pages
 
 """
+
 @app.route('/')
 @app.route('/index.html')
 def main():  # put application's code here
@@ -27,6 +42,13 @@ def fund():  # put application's code here
     return render_template("fund.html")
 
 
+# Uncomment the following if testing internalIndex.html with local MongoDB
+'''
+@app.route('/')
+@app.route('/internalIndex.html', methods = ['GET'])
+def internalIndex():  # put application's code here
+    return render_template("internalIndex.html", person = person) 
+'''
 
 if __name__ == '__main__':
     app.run()
