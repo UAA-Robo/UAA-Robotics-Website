9644d20f73c54a8918a09f842b09fd8aaca6b1ac
Patrick Pragman <patrickpragman@pop-os.localdomain>
2023-01-02 20:20:47 -0900

    skeleton of static page set up

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask, url_for, render_template, send_from_directory
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient()
 
 
+"""
+Static Pages
+
+"""
 @app.route('/')
+@app.route('/index.html')
 def main():  # put application's code here
-    return "Empty flask app"
+    return render_template("index.html")
 
-@app.route('/dev/')
-def dev_main():
-    return "Hello!"
+@app.route('/projects.html')
+def projects():  # put application's code here
+    return render_template("projects.html")
+
+@app.route('/outreach.html')
+def outreach():  # put application's code here
+    return render_template("outreach.html")
+
+@app.route('/fund.html')
+def fund():  # put application's code here
+    return render_template("fund.html")
+
 
 
 if __name__ == '__main__':
