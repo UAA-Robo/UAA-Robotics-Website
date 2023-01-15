89ad05f5806615c4d555baa70d0376a67be197bc
Patrick Pragman <patrickpragman@pop-os.localdomain>
2023-01-14 16:43:35 -0900

    formatting

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, url_for, render_template, send_from_directory, redirect
 from flask_session import Session
 from tempfile import mkdtemp
 from pymongo import MongoClient
-from bson.objectid import ObjectId #For working with PyMongo object Ids
+from bson.objectid import ObjectId  # For working with PyMongo object Ids
 
-##USE 'FLASK RUN' COMMAND WITHIN YOUR VENV TO RUN APP##
+# USE 'FLASK RUN' COMMAND WITHIN YOUR VENV TO RUN APP##
 
 app = Flask(__name__)
 
@@ -17,6 +17,7 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -25,41 +26,41 @@ app.config["SESSION_TYPE"] = "filesystem"
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-
-
-
-#This connects to our Atlas MongoDB. Your IP Adress will need to be added to the Newtork Access List on the Atlas website
+# This connects to our Atlas MongoDB. Your IP Adress will need to be added to the Newtork Access List on the Atlas website
 client = MongoClient(config["PROD"]["DB_URI"])
-db = client["uaa-robo"] #Same as db = client.uaa-robo (but python doesn't like the uaa-robo format)
+db = client["uaa-robo"]  # Same as db = client.uaa-robo (but python doesn't like the uaa-robo format)
 people = db.Person
 tasks = db.Task
 logs = db.Log
 
-#Alternative test customer db to use:
+# Alternative test customer db to use:
 '''
 db = client["sample_analytics"] #Alternative testing
 people = db.customers
-''' 
+'''
 
 Session(app)
-
 
 """
 Static Pages
 
 """
 
+
 @app.route("/")
 def main():  # put application's code here
     return render_template("home.html")
+
 
 @app.route("/projects", methods=["GET"])
 def projects():  # put application's code here
     return render_template("projects.html")
 
+
 @app.route("/outreach", methods=["GET"])
 def outreach():  # put application's code here
     return render_template("outreach.html")
+
 
 @app.route("/fund", methods=["GET"])
 def fund():  # put application's code here
@@ -71,14 +72,14 @@ Dynamic Pages
 
 """
 
-@app.route("/task_listing", methods = ["GET"])
+
+@app.route("/task_listing", methods=["GET"])
 def task_listing():  # put application's code here
-    return render_template("task_listing.html", people = people, tasks = tasks) 
+    return render_template("task_listing.html", people=people, tasks=tasks)
 
 
-@app.route("/tasks/<string:id>", methods = ["GET"])
-def task(id):  
-
+@app.route("/tasks/<string:id>", methods=["GET"])
+def task(id):
     singleTask = tasks.find_one(ObjectId(id))
 
-    return render_template("task.html", singleTask = singleTask, logs=logs, people = people) 
+    return render_template("task.html", singleTask=singleTask, logs=logs, people=people)
